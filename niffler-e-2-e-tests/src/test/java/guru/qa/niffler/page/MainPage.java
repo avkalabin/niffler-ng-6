b231bf4f3e0409b8a62c29c60b8dd9a27e74b0e9
Andrey Kalabin <122894684+avkalabin@users.noreply.github.com>
2024-11-06 22:19:50 +0500

    added hw-9_1 (#15)
    * added hw-9_1
    
    * fixed code after review

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/MainPage.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/MainPage.java
@@ -1,18 +1,30 @@
 package guru.qa.niffler.page;
 
+import com.codeborne.selenide.SelenideElement;
 import guru.qa.niffler.page.component.Header;
 import guru.qa.niffler.page.component.SpendingTable;
 import guru.qa.niffler.page.component.StatComponent;
+import guru.qa.niffler.utils.ScreenDiffResult;
 import io.qameta.allure.Step;
 
 import javax.annotation.Nonnull;
+import javax.imageio.ImageIO;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Objects;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.sleep;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class MainPage extends BasePage<MainPage> {
 
     public static final String URL = CFG.frontUrl() + "main";
+
+    private final SelenideElement statImg = $("canvas[role='img']");
+    private final SelenideElement statCell = $("#legend-container");
 
     protected final Header header = new Header();
     protected final SpendingTable spendingTable = new SpendingTable();
@@ -36,6 +48,25 @@ public class MainPage extends BasePage<MainPage> {
         header.getSelf().should(visible).shouldHave(text("Niffler"));
         statComponent.getSelf().should(visible).shouldHave(text("Statistics"));
         spendingTable.getSelf().should(visible).shouldHave(text("History of Spendings"));
+        return this;
+    }
+
+    @Step("Check that statistic image matches the expected image")
+    public MainPage checkStatImg(BufferedImage expected) throws IOException {
+        sleep(3000);
+        BufferedImage actual = ImageIO.read(Objects.requireNonNull(statImg.screenshot()));
+        assertFalse(new ScreenDiffResult(
+                actual,
+                expected
+        ));
+        return this;
+    }
+
+    @Step("Check that statistic cell have text {text}")
+    public MainPage checkStatCell(String... text) {
+        for (String s : text) {
+            statCell.shouldHave(exactText(s));
+        }
         return this;
     }
 }
